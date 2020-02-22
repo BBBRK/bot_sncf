441b7e68e9b4ca050ca6fe9d5fc34fa017bc2b48
Jimmy Gressier <jimg@laposte.net>
2020-02-22 12:17:26 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  host = 'localhost:3000'
+  host = 'https://bot-sncf.herokuapp.com/'
   config.action_mailer.default_url_options = {host: host}
 
   config.action_mailer.smtp_settings = {
