52b8b1915d1169f0934c0e9aca252d0d3899cfe9
Jimmy Gressier <jimg@laposte.net>
2020-02-28 14:01:41 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV['GMAILUSER'],
+    user_name:            "jimmy.bbbrk@gmail.com",
     password:             "dmpvwnahekunzwlv",
     # user_name:            "jimmy.bbbrk@gmail.com",
     # password:             "eterlol525",
